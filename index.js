b10edb4329db884f77c6e89c61de27d9f6f207e7
jordanlynne9@gmail.com <jordanlynne9@gmail.com>
2022-06-30 07:48:10 -0700

    commit tree 6c619593d8c56c7f4ea22fd3673eea3391e2fd9f

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+const multiply = (num1 * num2);
+
+console.log(multiply);
+
+
+const random = Math.floor(Math.random() * 5) + 3;
+
+Number.isInteger(random);
+
+let num3 = 12;
+let num4 = 3;
+
+const mod = (num3 / num4);
+
+Number.isInteger(mod);
+
+
+const max = Math.max(5, 10, 15, 20)
+
+
+
+
